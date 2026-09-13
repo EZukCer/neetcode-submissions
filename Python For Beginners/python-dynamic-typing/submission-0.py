10075f94c685bb55285c7785e5b304b34d67adaf
Erik Zukovskij <erikzukov@proton.me>
2026-09-13 15:38:52 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 35.3
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "kev"
+print(type(variable))
+variable = []
+print(type(variable))
