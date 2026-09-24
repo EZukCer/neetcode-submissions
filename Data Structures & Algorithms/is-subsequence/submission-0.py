93b2001b049bd235736b645090c8603ea3810aa6
Erik Zukovskij <erikzukov@proton.me>
2026-09-24 13:49:54 +0100

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        k = 0
+
+        for c in t:
+            if k < len(s) and s[k] == c:
+                k += 1
+        
+        return k == len(s)
+
